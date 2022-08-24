5277a6c653e9335e9180430b78cab50a8cdbc41d
M4p4 <homejr@protonmail.com>
2022-08-24 09:46:55 +0200

    added fonts , settings

--- grid/tailwind.config.js
+++ grid/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      fontFamily: {
+        sans: ['Barlow Semi Condensed', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
