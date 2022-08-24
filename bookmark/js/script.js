488016082ce64585af99b0b6ab5f2d3c80074aaa
M4p4 <homejr@protonmail.com>
2022-08-24 17:58:50 +0200

    finish bookmark site

--- bookmark/js/script.js
+++ bookmark/js/script.js
@@ -1,5 +1,20 @@
 const tabs = document.querySelectorAll('.tab');
 const panels = document.querySelectorAll('.panel');
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+const logo = document.getElementById('logo');
+
+const navToggle = () => {
+  btn.classList.toggle('open');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('hidden');
+
+  if (menu.classList.contains('flex')) {
+    logo.setAttribute('src', './images/logo-bookmark-footer.svg');
+  } else {
+    logo.setAttribute('src', './images/logo-bookmark.svg');
+  }
+};
 
 const onTabClick = (e) => {
   tabs.forEach((tab) => {
@@ -20,6 +35,8 @@ const onTabClick = (e) => {
   const selectedPanel = document.querySelector(`.${panelClass}`);
   selectedPanel.classList.remove('hidden');
 };
+
+btn.addEventListener('click', navToggle);
 
 tabs.forEach((tab) => {
   tab.addEventListener('click', onTabClick);
