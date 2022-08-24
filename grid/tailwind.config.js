f3d8f3c39a975d80bb991ddc02bcd54eb3ff3e5f
M4p4 <homejr@protonmail.com>
2022-08-24 10:54:25 +0200

    wip fixed config for project

--- grid/tailwind.config.js
+++ grid/tailwind.config.js
@@ -2,13 +2,13 @@
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
-      screens: {
-        sm: '480px',
-        md: '768px',
-        lg: '976px',
-        xl: '1440px',
-      },
       fontFamily: {
         sans: ['Barlow Semi Condensed', 'sans-serif'],
       },
